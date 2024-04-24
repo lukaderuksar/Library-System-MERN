e49e340ceb62307dfc974a1cf7083fe3f9e6b807
Mokesh <mokeshb2002@gmail.com>
2024-04-24 00:47:12 -0400

    update mongoose schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,16 +2,32 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  userType: String,
+  userType: {
+    type: String,
+    enum: ["student", "employee"], // Only allow "student" or "employee"
+    required: true
+  },
   userFullName: String,
-  admissionId: String,
-  employeeId: String,
+  admissionId: {
+    type: String,
+    unique: true, // Ensure admissionId is unique
+    sparse: true // Allow null or missing admissionId
+  },
+  employeeId: {
+    type: String,
+    unique: true, // Ensure employeeId is unique
+    sparse: true // Allow null or missing employeeId
+  },
   age: Number,
   dob: Date,
   gender: String,
   address: String,
   mobileNumber: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true, // Ensure email is unique
+    required: true
+  },
   password: String,
   isAdmin: Boolean,
 });
